574a10b02de6ff55ea03f680b82abaf59c471da6
villysiu <villysiu@gmail.com>
2021-06-10 15:32:56 -0700

    add item to cart and reflect adition without refreshpage, add serialized json to new line_item

--- grocery-backend/app/controllers/line_items_controller.rb
+++ grocery-backend/app/controllers/line_items_controller.rb
@@ -2,18 +2,14 @@ class LineItemsController < ApplicationController
   
 
 def create
-  cart = Cart.find(params[:cart_id])
-  cart.add_item(params[:item_id], params[:quantity])
-
-  render json: { status: 'SUCCESS', message: 'Item added to cart.' }, status: :created
+  # cart = Cart.find(params[:cart_id])
+  
+  line_item = LineItem.create(cart_id: params[:cart_id], item_id: params[:item_id], quantity: params[:quantity])
+  render json: LineitemSerializer.new(line_item).to_serialized_json
+  
   end
   
-  def update
-    cart = Cart.find(params[:cart_id])
-        cart.update_cart_item(params[:item_id], params[:quantity])
-
-        render json: { status: 'SUCCESS', message: 'Item quantity updated.' }, status: :ok
-  end
+ 
 
   def destroy
     render json: LineItem.find(params[:id]).destroy
