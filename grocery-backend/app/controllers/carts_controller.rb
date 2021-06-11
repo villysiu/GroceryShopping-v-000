3375b3c8b8a3b4a75c9d43c086f0b5582971e384
villysiu <villysiu@gmail.com>
2021-06-10 21:19:35 -0700

    after delete item in lineitem controller, return to cartcontroller show cart

--- grocery-backend/app/controllers/carts_controller.rb
+++ grocery-backend/app/controllers/carts_controller.rb
@@ -7,17 +7,11 @@ class CartsController < ApplicationController
   end
     def show
         cart = Cart.find(params[:id])
-         render json: CartSerializer.new(cart).to_serialized_json
+        render json: CartSerializer.new(cart).to_serialized_json
          
         #render json: cart
     end
-    def create
-        #cart = Cart.create
-      end
-    
-      def destroy
-        render json: Cart.find(params[:id]).destroy
-      end
+   
     
      
 
